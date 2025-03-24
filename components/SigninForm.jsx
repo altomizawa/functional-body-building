dbeef7c31d147a53bd49d5cb9704873c259cf88f
altomizawa <alyssontomizawa@hotmail.com>
2025-03-24 07:29:46 -0300

    quickfix: add isPending

--- components/SigninForm.jsx
+++ components/SigninForm.jsx
@@ -7,7 +7,7 @@ import { redirect } from 'next/navigation'
 
 export function SigninForm() {
   const [showPassword, setShowPassword] = useState(false)
-  const [state, formAction, pending] = useActionState(signinUser)
+  const [state, formAction, isPending] = useActionState(signinUser)
   
   if (state?.success) {
     localStorage.setItem('token', JSON.stringify(state.token))
@@ -25,7 +25,7 @@ export function SigninForm() {
           <input type={showPassword ? "text" : "password"} id="password" name="password" className="block w-full px-3 py-2 placeholder-gray-400 transition duration-150 ease-in-out border border-gray-300 rounded-md shadow-sm appearance-none focus:outline-none focus:shadow-outline-blue focus:border-blue-300 sm:text-sm sm:leading-5" placeholder="*************" />
           <button onClick={() => setShowPassword(prev => !prev)} type='button' className='underline text-sm'>show password</button>
         </label>
-        <button type='submit' className='w-full border-2 py-2 rounded-md bg-black text-white hover:bg-gray-500'>{pending ? "Submitting" : "Let me in"}</button>
+        <button type='submit' className='w-full border-2 py-2 rounded-md bg-black text-white hover:bg-gray-500'>{isPending ? "Submitting" : "Let me in"}</button>
         {!state?.success && <p className="text-red-500 text-sm">{state?.error}</p>}
       </div>
     </form>
