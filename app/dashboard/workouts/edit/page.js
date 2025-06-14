2cd79df1936696846176228e6e7b987a64097bbd
altomizawa <alyssontomizawa@hotmail.com>
2025-06-13 22:11:56 -0300

    fix: improve workout form and data handling
    This commit addresses several issues and improves the workout form and data handling:
    
    - Removes unnecessary population of movements in `fetchWorkout` action to improve efficiency.
    - Refactors the workout form in `app/dashboard/workouts/edit/page.js` to simplify state management and section handling.
    - Fixes issues with adding and removing movements in the workout form.
    - Implements dynamic section rendering and management in `app/dashboard/workouts/add/page.js`, including adding, removing, and selecting sections.
    - Improves movement search and selection in the workout form.
    - Adds error handling and user feedback for workout creation and data fetching.
    - Enhances the overall user experience of the workout form.

--- app/dashboard/workouts/edit/page.js
+++ app/dashboard/workouts/edit/page.js
@@ -3,40 +3,31 @@
 import { useState, useEffect, useRef } from "react"
 import Preview from '@/components/Preview'
 import Link from 'next/link'
-import { createWorkout, getAllMovements } from '@/lib/actions'
+import  { createWorkout, getAllMovements } from '@/lib/actions'
 import { Toaster } from "@/components/ui/toaster"
 import { useToast } from '@/hooks/use-toast'
 
 
 
-export default function EditWorkoutForm() {
+export default function PillarWorkoutForm() {
   const [movements, setMovements] = useState([])
   const [filteredMovements, setFilteredMovements] = useState(null)
-  const [currentSection, setSection] = useState(0)
 
   const [newWorkout, setNewWorkout] = useState({
     program: '',
     week: '',
     day: '',
-    sections: [
-      {
-        section: '',
-        icon: '',
-        description: '',
-        movements: [],
-        notes: ''
-      },
-      {
-        section: '',
-        icon: '',
-        description: '',
-        movements: [],
-        notes: ''
-      }
-    ]
+    sections: []
+  })
+  const [section, setSection] = useState({
+    section: '',
+    icon: '',
+    description: '',
+    movements: [],
+    notes: ''
   })
 
-  const toast = useToast().toast
+   const toast = useToast().toast
 
   // RESET ALL FORMS
   const resetForm = () => {
@@ -44,20 +35,24 @@ export default function EditWorkoutForm() {
       program: '',
       week: '',
       day: '',
-      sections: [{
-        section: '',
-        icon: '',
-        description: '',
-        movements: [],
-        notes: ''
-      }]
+      sections: []
+    })
+
+    setSection({
+      section: '',
+      icon: '',
+      description: '',
+      movements: [],
+      notes: ''
     })
   }
 
   const movementInputRef = useRef(null)
+  
+
 
   const onSubmit = async (e) => {
-    e.preventDefault()
+    e.preventDefault();
     const response = await createWorkout(newWorkout)
     if (!response.success) {
       toast({
@@ -74,17 +69,19 @@ export default function EditWorkoutForm() {
   }
 
   const handleWorkoutChange = (e) => {
-    setNewWorkout(prev => ({
-      ...prev,
-      [e.target.name]: e.target.value
-    }))
+    setNewWorkout( prev => {
+      return {
+        ...prev,
+        [e.target.name]: e.target.value
+      }
+    })
   }
 
   const handleSectionChange = (e) => {
     setSection(prev => ({
       ...prev,
       [e.target.name]: e.target.value
-    }))
+    }));
   }
 
   // ADD MOVEMENT TO SECTION
@@ -93,32 +90,25 @@ export default function EditWorkoutForm() {
       setFilteredMovements(null)
       return
     }
-    setFilteredMovements(
-      movements.filter(movement =>
-        movement.name.includes(e.target.value.toLowerCase())
-      )
-    )
-  }
+    setFilteredMovements(movements.filter(movement => movement.name.includes(e.target.value.toLowerCase())))  }
 
   const addMovement = (movement) => {
     setSection(prevSection => ({
-      ...prevSection,
+      ...prevSection, 
       movements: [...prevSection.movements, movement]
-    }))
+    }));
     setFilteredMovements(null)
     movementInputRef.current.value = ''
-  }
-
+  };
   const removeMovement = (movement) => {
     setSection(prevSection => ({
-      ...prevSection,
-      movements: prevSection.movements.filter(
-        (prevMovement) => prevMovement.name !== movement.name
-      )
-    }))
-  }
+      ...prevSection, 
+      movements: prevSection.movements.filter((prevMovement) => prevMovement.name !== movement.name)
+    }));
+  };
 
   const addNewSection = () => {
+
     setNewWorkout(prev => ({
       ...prev,
       sections: [...prev.sections, section]
@@ -149,147 +139,76 @@ export default function EditWorkoutForm() {
       }
     }
     fetchMovements()
-  }, [])
+  }
+  , [])
 
   return (
-    <form onSubmit={onSubmit} className='h-full px-6 my-16 max-w-[1440px] mx-auto'>
-      {/* FORM */}
-      <div>
-        <Toaster />
-        <h1 className="text-2xl md:text-3xl font-bold text-left">ADD NEW WORKOUT</h1>
-        <div className='flex items-center gap-2 mt-4'>
-          {/* <label htmlFor="program">Program: </label> */}
-          <input
-            className='w-full'
-            onChange={handleWorkoutChange}
-            type="text"
-            name="program"
-            placeholder="Program"
-            value={newWorkout.program}
-            required
-          />
-          {/* <label htmlFor="week">Week:</label> */}
-          <input
-            className='w-16'
-            onChange={handleWorkoutChange}
-            type="number"
-            name="week"
-            placeholder="Week"
-            value={newWorkout.week}
-            min={1}
-            required
-          />
-          {/* <label htmlFor="day">Day:</label> */}
-          <input
-            className='w-16'
-            onChange={handleWorkoutChange}
-            type="number"
-            name="day"
-            placeholder="Day"
-            value={newWorkout.day}
-            max={7}
-            min={1}
-            required
-          />
-        </div>
+      <form onSubmit={onSubmit} className='flex flex-col md:grid md:grid-cols-2 gap-12 h-full px-6 my-16 max-w-[1440px] mx-auto'>
+        {/* FORM */}
+        <div>
+          <Toaster />
+          <h1 className="text-2xl md:text-3xl font-bold text-left">ADD NEW WORKOUT</h1>
+          <div className='flex items-center gap-2 mt-4'>
+            {/* <label htmlFor="program">Program: </label> */}
+            
+            <input className='w-full' onChange={handleWorkoutChange} type="text" name="program" placeholder="Program" value={newWorkout.program} required />
+            {/* <label htmlFor="week">Week:</label> */}
+            <input className='w-16' onChange={handleWorkoutChange} type="number" name="week" placeholder="Week" value={newWorkout.week} min={1} required />
+            {/* <label htmlFor="day">Day:</label> */}
+            <input className='w-16' onChange={handleWorkoutChange} type="number" name="day" placeholder="Day" value={newWorkout.day} max={7} min={1} required />
+          </div>
 
-        <div id='sections' className='my-4 backdrop:border-2 space-y-24'>
-          {/* SECTION */}
-          {newWorkout.sections && newWorkout.sections.map((section, index) => (
-            <div className='space-y-8 border-2 p-6' key={index}>
-            <div className='w-full space-y-2'>
-              <label htmlFor="section">SECTION NAME:</label>
-              <input
-                className='w-full'
-                onChange={handleSectionChange}
-                type="text"
-                name="section"
-                placeholder="Section"
-              />
-            </div>
-            <div className='w-full space-y-2'>
-              <label htmlFor="description">DESCRIPTION:</label>
-              <textarea
-                className='w-full min-h-48 p-2 border-2'
-                onChange={handleSectionChange}
-                type="text"
-                name="description"
-                placeholder="Description"
-                value={'section description'}
-              />
-            </div>
-            <div className='w-full space-y-2'>
-              <label htmlFor="section">ADD MOVEMENTS TO SECTION:</label>
-              <input
-                className='w-full'
-                onChange={handleMovement}
-                type="text"
-                name="movements"
-                placeholder="Movements"
-                ref={movementInputRef}
-              />
-              <div className='flex flex-wrap gap-4'>
-                {/* {section.movements && section.movements.map((movement, index) => (
-                  <p key={index} className='border-[1px] border-black/20 px-2 cursor-pointer hover:bg-red-500 hover:text-white' onClick={() => removeMovement(movement)}>{movement.name}<span className='ml-2 cursor-pointer' onClick={()=>removeMovement(movement)}>X</span></p>
-                ))} */}
+          <div id='sections' className='py-4  my-4'>
+            {/* SECTION */}
+            <div className='space-y-8'>
+              <div className='w-full space-y-2'>
+                <label htmlFor="section">SECTION NAME:</label>
+                <input className='w-full' onChange={handleSectionChange} type="text" name="section" placeholder="Section" value={section.section} />
               </div>
-              <div className='relative'>
-                {filteredMovements && filteredMovements.length > 0 && (
-                  <ul className='absolute top-4 left-0 bg-white border-[1px] border-black/20 shadow-md'>
+              <div className='w-full space-y-2'>
+                <label htmlFor="description">DESCRIPTION:</label>
+                <textarea className='w-full min-h-48 p-2 border-2' onChange={handleSectionChange} type="text" name="description" placeholder="Description" value={section.description} />
+              </div>
+              <div className='w-full space-y-2'>
+                <label htmlFor="section">ADD MOVEMENTS TO SECTION:</label>
+                <input className='w-full' onChange={handleMovement} type="text" name="movements" placeholder="Movements" ref={movementInputRef}/>
+                <div className='flex flex-wrap gap-4'>
+                  {section.movements && section.movements.map((movement, index) => (
+                    <p key={index} className='border-[1px] border-black/20 px-2 cursor-pointer hover:bg-red-500 hover:text-white' onClick={() => removeMovement(movement)}>{movement.name}<span className='ml-2 cursor-pointer' onClick={()=>removeMovement(movement)}>X</span></p>
+                  ))}
+                </div>
+                <div className='relative'>
+                  {filteredMovements && filteredMovements.length>0 && <ul className='absolute top-4 left-0 bg-white border-[1px] border-black/20 shadow-md'>
                     {filteredMovements?.map((movement, index) => (
-                      <div
-                        key={index}
-                        className='px-2 py-2 flex items-center gap-2 cursor-pointer hover:bg-black hover:text-white'
-                        onClick={() => addMovement(movement)}
-                      >
+                      <div key={index} className='px-2 py-2 flex items-center gap-2 cursor-pointer hover:bg-black hover:text-white' onClick={() => addMovement(movement)}>
                         <p>{movement.name}</p>
                       </div>
                     ))}
-                  </ul>
-                )}
+                  </ul>}
+                </div>
+              </div>
+              <div className='w-full space-y-2'>
+                <label htmlFor="notes">NOTES:</label>
+                <textarea className='w-full min-h-24 p-2 border-2' onChange={handleSectionChange} type="text" name="notes" placeholder="Notes" value={section.notes} />
               </div>
             </div>
-            <div className='w-full space-y-2'>
-              <label htmlFor="notes">NOTES:</label>
-              <textarea
-                className='w-full min-h-24 p-2 border-2'
-                onChange={handleSectionChange}
-                type="text"
-                name="notes"
-                placeholder="Notes"
-              />
-            </div>
-          <button
-            type='button'
-            className='button__main-menu my-2 uppercase'
-            onClick={addNewSection}
-          >
-            {newWorkout.sections.length === index+1 ? 'add section' : 'remove section'}
-          </button>
-          </div>))}
-        </div>
-      </div>
+            <button type='button' className='button__main-menu my-2 uppercase' onClick={addNewSection}>add section</button>
+          </div>
 
-      <div>
-        <h1 className="text-2xl md:text-3xl font-bold text-left mx-auto">PREVIEW WORKOUT</h1>
-        <div className='h-max overflow-auto border-[1px] border-gray-600 rounded-lg mt-4'>
-          <Preview workout={newWorkout} />
         </div>
-        <div className='flex flex-col gap-4 mt-4'>
-          <Link href="/" className='button__back flex justify-center items-center'>
-            <span className="material-symbols-outlined">arrow_back</span>BACK
-          </Link>
-          <button className='button__submit'>SUBMIT</button>
+        <div>
+          <h1 className="text-2xl md:text-3xl font-bold text-left mx-auto">PREVIEW WORKOUT</h1>
+          <div className='h-max overflow-auto border-[1px] border-gray-600 rounded-lg mt-4'>
+            <Preview workout={newWorkout} />
+          </div>
+          <div className='flex flex-col gap-4 mt-4'>
+            <Link href="/" className='button__back flex justify-center items-center'>
+              <span className="material-symbols-outlined">arrow_back</span>BACK
+            </Link>
+            <button className='button__submit'>SUBMIT</button>
+          </div>
         </div>
-      </div>
-
-      {/* <div className='flex flex-col gap-4 mt-4'>
-        <Link href="/" className='button__back flex justify-center items-center'>
-          <span className="material-symbols-outlined">arrow_back</span>BACK
-        </Link>
-        <button className='button__submit'>SUBMIT</button>
-      </div> */}
-    </form>
+      </form>
   )
 }
 
