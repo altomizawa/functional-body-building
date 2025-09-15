d5a957f1cde7a6808bc8c805ad0e76d4528bd259
Al Tomizawa <alyssontomizawa@hotmail.com>
2025-09-15 14:46:30 -0300

    feat(SignUp): implement session verification and redirect for non-admin users

--- app/signup/page.js
+++ app/signup/page.js
@@ -2,10 +2,18 @@ import React from 'react'
 import { SignupForm } from '@/components/SignupForm'
 import KOR__logo from '@/public/images/KOR_Somente_Red.svg'
 import Image from 'next/image'
+import { redirect } from 'next/navigation'
+import { verifySession } from '@/lib/session'
 
-const SignUp = () => {
+const SignUp = async () => {
+  const session = await verifySession()
+  console.log('role: ', session?.role)
+  if (session.role !== 'admin') redirect('/programs/pillars')
+      
+
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-4 bg-gray-50">
+    <main className="flex min-h-screen flex-col items-center justify-center p-4">
       <div className='absolute top-8 lef-0 w-full'>
         <Image src={KOR__logo} alt="logo" width={200} height={200} className="w-16 md:w-[120px] h-auto mx-auto" />
         <h1 className="text-sm md:text-xl text-center">FUNCTIONAL BODYBUILDING</h1>
