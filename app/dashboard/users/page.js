5628372ddd96da35b0a189bea9128d5b93ef5ed1
Al Tomizawa <alyssontomizawa@hotmail.com>
2026-07-20 23:42:49 -0300

    feat: add SignupForm to users dashboard and apply code style improvements

--- app/dashboard/users/page.js
+++ app/dashboard/users/page.js
@@ -1,11 +1,13 @@
 "use client"
 
 import { useState, useRef } from "react"
-import { findUserByName} from '@/lib/actions'
+import { findUserByName } from '@/lib/actions'
 import { modifyUser } from '@/lib/actions'
 import useToast from '@/contexts/useToast'
 import EditUserForm from '@/components/users/EditUserForm'
 import Dropdown from '@/components/form/Dropdown'
+import { debounce } from '@/utils/debounce'
+import { SignupForm } from '@/components/SignupForm'
 
 export default function Users() {
   const [selectedUser, setSelectedUser] = useState(null)
@@ -16,11 +18,11 @@ export default function Users() {
 
   const searchInputRef = useRef(null)
 
-  const handleInputChange = debounce( async (e) => {
+  const handleInputChange = debounce(async (e) => {
     if (e.target.value.trim() === "") return setUsers([])
     if (e.target.value.length < 3) return
-    
-    try{
+
+    try {
       const response = await findUserByName(e.target.value);
       if (!response.success) {
         throw new Error(response.error)
@@ -33,19 +35,19 @@ export default function Users() {
   }, 500)
 
 
-  const handleSubmit = async(formdata) => {
+  const handleSubmit = async (formdata) => {
     const data = {
       name: formdata.get('name'),
       email: formdata.get('email'),
       phone: formdata.get('phone'),
       status: formdata.get('status'),
       id: formdata.get('id')
-    }    
+    }
     const response = await modifyUser(data)
     if (response.success) {
       setSelectedUser(null)
       setToasts((prev) => [
-        ...prev, 
+        ...prev,
         {
           title: "Success",
           description: `${response.data.name} modified successfully`,
@@ -58,11 +60,11 @@ export default function Users() {
     setToasts((prev) => [
       ...prev,
       {
-      title: 'Error',
-      description:`${response.error}, status: ${response.status}`,
-      variant: 'error',
-      duration: 5000
-    }])
+        title: 'Error',
+        description: `${response.error}, status: ${response.status}`,
+        variant: 'error',
+        duration: 5000
+      }])
   }
   const addUser = (user) => {
     setSelectedUser(user)
@@ -72,37 +74,38 @@ export default function Users() {
   }
 
   return (
-      <div className='flex flex-col md:grid md:grid-cols-2 gap-12 h-full px-6 my-16 max-w-[1440px] mx-auto'>
-        {/* FORM */}
-        <div>
-          <h1 className="text-2xl md:text-3xl font-bold text-left">USERS</h1>
-          <div className='flex flex-col gap-2 mt-4 relative'>
-            <label htmlFor="userSearch">SEARCH USER BY NAME: </label>
-            <input 
-              id="userSearch"
-              onChange={handleInputChange}
-              className='w-full' 
-              type="text" 
-              placeholder="Type user name" 
-              required 
-              autoComplete='off'
-              ref={searchInputRef}
-            />
-            <div className='relative'>
-              {users.length > 0 && (
-                <Dropdown 
-                  array={users} 
-                  action={addUser}
-                />
-              )}
+    <div className='flex flex-col md:grid md:grid-cols-2 gap-12 h-full px-6 my-16 max-w-[1440px] mx-auto'>
+      {/* FORM */}
+      <div>
+        <h1 className="text-2xl md:text-3xl font-bold text-left">USERS</h1>
+        <div className='flex flex-col gap-2 mt-4 relative'>
+          <label htmlFor="userSearch">SEARCH USER BY NAME: </label>
+          <input
+            id="userSearch"
+            onChange={handleInputChange}
+            className='w-full'
+            type="text"
+            placeholder="Type user name"
+            required
+            autoComplete='off'
+            ref={searchInputRef}
+          />
+          <div className='relative'>
+            {users.length > 0 && (
+              <Dropdown
+                array={users}
+                action={addUser}
+              />
+            )}
 
-            </div>
           </div>
         </div>
-        <div>
-          <h1 className="text-2xl md:text-3xl font-bold text-left mx-auto">USER INFO</h1>
-          <EditUserForm selectedUser={selectedUser} handleSubmit={handleSubmit} status={status} setStatus={setStatus}/>
-        </div>
       </div>
+      <div>
+        <h1 className="text-2xl md:text-3xl font-bold text-left mx-auto">USER INFO</h1>
+        <EditUserForm selectedUser={selectedUser} handleSubmit={handleSubmit} status={status} setStatus={setStatus} />
+      </div>
+      <SignupForm />
+    </div>
   )
 }
