314941e163788a8dfdc54d337d36ab0ac70ec08c
Al Tomizawa <alyssontomizawa@hotmail.com>
2026-09-07 15:47:07 -0300

    style: hide back button in /programs for non-admin

--- app/programs/page.js
+++ app/programs/page.js
@@ -1,6 +1,16 @@
 import Link from 'next/link'
+import { redirect } from 'next/navigation'
+import { verifySessionForRequests } from '@/lib/session'
+import { getUserById } from '@/lib/actions'
 
 const programs = async () => {
+  // Verify session
+  const session = await verifySessionForRequests();
+  if (!session) {
+    redirect('/login');
+  }
+
+  console.log('session : ', session)
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
@@ -9,15 +19,11 @@ const programs = async () => {
         <div className='w-full flex flex-col gap-4'>
           <Link href="/programs/pillars" className='button__main-menu bg-white'>PILLARS</Link>
           <Link href="/programs/pump" className='button__main-menu bg-white'>PUMP 4x</Link>
-          <Link href="/" className='button__back flex items-center gap-2 justify-center'>
+          {session.user.role === 'admin' && <Link href="/" className='button__back flex items-center gap-2 justify-center'>
             <span className="material-symbols-outlined">arrow_back</span>BACK
-          </Link>
-
+          </Link>}
         </div>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <p className="text-sm text-gray-500">footer</p>
-      </footer>
     </div>
   )
 }
